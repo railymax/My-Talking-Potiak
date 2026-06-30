cb86ede7f2f90b164d5726f15dd0cb06499ed76c
railymax <sadrinmaks05@gmail.com>
2026-07-01 00:53:12 +0400

    MTP0107.0053

---
+++ Assets/Scripts/Bg.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Bg : MonoBehaviour
+{
+    public Image backgroundImage;
+    public Color gameRoomcolor = Color.gray;
+    public Color kitchenRoomcolor = Color.green;
+    public Color bathRoomcolor = Color.blue;
+    public Color sleepRoomcolor = Color.magenta;
+    public Color factoryRoomcolor = Color.magenta;
+
+    public void SetBackgroundColor(string roomName)
+    {
+        if (backgroundImage == null) return;
+
+        switch (roomName.ToLower())
+        {
+            case "game":
+                backgroundImage.color = gameRoomcolor;
+                break;
+            case "kitchen":
+                backgroundImage.color = kitchenRoomcolor;
+                break;
+            case "bath":
+                backgroundImage.color = bathRoomcolor;
+                break;
+            case "sleep":
+                backgroundImage.color = sleepRoomcolor;
+                break;
+            case "factory":
+                backgroundImage.color = factoryRoomcolor;
+                break;
+        }
+    }
+}
+
